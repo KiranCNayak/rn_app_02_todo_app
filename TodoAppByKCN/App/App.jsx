1674c289b4fadc8c6d7f1cee0499e43a0cf699f2
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-08-29 15:38:28 +0530

    • Renamed App.tsx to App.jsx • Moved App.jsx to 'TodoAppByKCN/App' directory • Added styled-components library for styling

---
+++ TodoAppByKCN/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Text>{'Kiran Nayak'}</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
