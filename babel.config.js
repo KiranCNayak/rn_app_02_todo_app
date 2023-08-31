1125a30f515e1ce8c819a97fc3d73c19802a66bc
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-08-31 11:52:33 +0530

    Added react-native-reanimated library • Also ran `pod install` from `ios/` folder

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  // Remember to always have 'react-native-reanimated/plugin' at the end of this array.
+  plugins: ['react-native-reanimated/plugin'],
 };
