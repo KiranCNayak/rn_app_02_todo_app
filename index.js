c05a77bb8109ff30b426027a3b8589a96bd81d5d
Kiran C Nayak <33034799+KiranCNayak@users.noreply.github.com>
2023-09-01 01:55:31 +0530

    Followed all the steps mentioned in the library • Ran 'npx rnn-link' and 'pod install' to link files to native code

--- index.js
+++ index.js
@@ -3,8 +3,23 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import App from './TodoAppByKCN/App/App';
-import {name as appName} from './app.json';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.myApp.WelcomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
